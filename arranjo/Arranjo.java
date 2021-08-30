beaae634a16f619976736638b33bd9a61db1371f
putsbam <alessandroosf@hotmail.com>
2021-08-30 14:24:26 -0300

    Questao 3, Lista 3 Resolvidas!

--- arranjo/Arranjo.java
+++ arranjo/Arranjo.java
@@ -25,4 +25,103 @@ public class Arranjo<T> {
         System.out.println("]");
     }
 
+
+    // Editado a partir daqui
+
+    // Lista 03
+
+    // Q.3 - a) Método para inserir na ultima posicao nao ocupada do arranjo
+
+    public void q3a(T data){
+
+        int n = 0; // num da posicao
+
+        for (int i = 0; i < this.Data.length; i++){ // loop pelo array inteiro, incrementando +1
+
+            n=i; // n recebe i, que foi incrementado ate chegar na ultima posicao do array
+
+        }
+        if(this.Data[n]==null){ // caso a data do array na posicao "n" seja null (ultima posicao)
+
+            this.Data[n] = data; // recebe o valor data
+
+        }
+
+    }
+
+    // Q.3 - b) Método para inserir na primeira posicao do arranjo
+
+    public void q3b (T dado) {
+        //checar se o array está cheio criando um contador de espaços preenchidos
+        int cont = 0;
+        for (int i=0; i < this.Data.length; i++) {
+            if (this.Data[i] == null) {
+                break;
+            } else {
+                cont++;
+            }
+        }
+        if (cont <= (this.Data.length)) { //se não estiver cheio, mover os valores e setar o valor no index 0
+            for (int j = this.Data.length-1; j > 0; j--){
+                this.Data[j] = this.Data[j-1];
+            }
+            Set(0, dado);
+        } else { //se estiver cheio, disparar exceção
+            System.out.println("O arranjo já está preenchido! Não é possível inserir novos dados.");
+        }
+    }
+
+    // Q.3 - c) Método para inserir um dado em uma dada posição do arranjo
+
+    public void q3c(int p, T obj) {
+        T a;
+        T b;
+        for (int i=0; i < this.Data.length; i++) {
+            if (p == i) {
+                a = this.Data[i];
+                this.Data[i] = obj;
+                for (int j=1+p; j < this.Data.length; j++){
+                    b = this.Data[j];
+                    this.Data[j] = a;
+                    a = b;
+                }
+            }
+        }
+    }
+
+    // Q.3 - d) Método para remover e retornar o dado da última posição ocupada do arranjo
+
+    public T q3d(){
+        int dataLength = this.Data.length;
+        T data = this.Data[dataLength-1];
+        this.Data[dataLength-1] = null;
+        return data;
+    }
+
+
+    // Q.3 - e) Método para remover e retornar o dado da primeira posição do arranjo
+
+    public T q3e () {
+        T dado = this.Data[0];
+
+        for (int j=1; j < this.Data.length; j++){
+            this.Data[j-1] = this.Data[j];
+        }
+        Set((this.Data.length-1), null);
+        return dado;
+    }
+
+    // Q.3 - f) Método para remover o dado da posição determinada do arranjo
+
+    public void q3f(int p) {
+        for (int i=0; i < this.Data.length; i++) {
+            if (p == i) {
+                for (int j=1+p; j < this.Data.length; j++){
+                    this.Data[j-1] = this.Data[j];
+                }
+            }
+        }
+    }
+
 }
+
--- arranjo/TesteArranjo.java
+++ arranjo/TesteArranjo.java
@@ -1,7 +1,15 @@
 package arranjo;
 
+import java.util.Scanner;
+import java.lang.Thread;
+
+import static java.lang.Integer.parseInt;
+
 public class TesteArranjo {
+
     public static void main(String[] args) {
+
+        Scanner scanned = new Scanner(System.in);
 
 		Arranjo<String> arr1 = new Arranjo<String>(5);
 		
@@ -12,5 +20,84 @@ public class TesteArranjo {
         arr1.Imprime();
 		arr1.Set(3,"d");
         arr1.Imprime();
-	}   
+
+        // Begin questao 3
+
+        /*
+        a) Inserir um dado na última posição não ocupada do arranjo. O dado é parâmetro do método.
+        */
+
+        System.out.println("\n\tQ.3 - a)\n");
+        System.out.println("Qual dado a ser inserido na ultima posiçao do arranjo?");
+        arr1.q3a(scanned.nextLine());
+        arr1.Imprime();
+
+        sleepMethod(2000);
+
+        /*
+        b) Inserir um dado na primeira posição do arranjo. Movendo o conteudo e preservando os dados
+        */
+
+        System.out.println("\n\tQ.3 - b)\n");
+        System.out.println("Qual dado a ser inserido na primeira posiçao arranjo?");
+        arr1.q3b(scanned.nextLine());
+        arr1.Imprime();
+
+        sleepMethod(2000);
+
+        /*
+        c) Inserir um dado em uma dada posição do arranjo
+        */
+
+        System.out.println("\n\tQ.3 - d)\n");
+        System.out.println("Qual posiçao voce quer inserir o dado de teste 't'?");
+        arr1.q3c(parseInt(scanned.nextLine()), "t");
+        arr1.Imprime();
+
+        sleepMethod(2000);
+
+        /*
+        d) Remover e retornar o dado da última posição ocupada do arranjo. Movendo o conteudo e preservando os dados
+        */
+
+        System.out.println("\n\tQ.3 - d)\n");
+        System.out.println("Removendo ultimo dado...");
+        System.out.println(arr1.q3d() + " foi removido!");
+        arr1.Imprime();
+
+        sleepMethod(2000);
+
+        /*
+        e) Remover e retornar o dado da primeira posição ocupada do arranjo. Movendo o conteudo e preservando os dados
+        */
+
+        System.out.println("\n\tQ.3 - e)\n");
+        System.out.println("Removendo primeiro dado...");
+        System.out.println(arr1.q3e() + " foi removido!");
+        arr1.Imprime();
+
+        sleepMethod(2000);
+
+        /*
+        f) Remover o dado da posição determinada do arranjo. Movendo o conteudo e preservando os dados
+        */
+
+        System.out.println("\n\tQ.3 - f)\n");
+        System.out.println("Qual posiçao voce quer remover o dado?");
+        arr1.q3f(parseInt(scanned.nextLine()));
+        arr1.Imprime();
+
+        sleepMethod(2000);
+	}
+
+    static void sleepMethod(int time) { // function só pra ter um timeout entre as questoes
+
+        try {
+            Thread.sleep(time);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
 }
+
